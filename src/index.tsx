7bb17d0e300713c1cb621c5c9a2c834177bcb063
nullkal <nullkal@nil.nu>
2023-12-11 00:01:34 +0900

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { basicAuth } from 'hono/basic-auth'
 import { logger } from 'hono/logger'
 import ky from 'ky'
 import * as fs from 'fs'
+import { PrismaClient } from '@prisma/client'
+import { url } from 'inspector'
 
 const USERNAME = process.env.USERNAME || 'nullkal'
 const DOMAIN = process.env.DOMAIN || 'localhost'
@@ -15,6 +17,7 @@ const PUBLIC_KEY = fs.readFileSync('./data/public.pem', 'utf8')
 const PRIVATE_KEY = fs.readFileSync('./data/private.pem', 'utf8')
 
 const app = new Hono()
+const prisma = new PrismaClient()
 
 app.use('*', logger())
 app.use('/static/*', serveStatic({ root: './' }))
@@ -24,7 +27,14 @@ const auth = basicAuth({
   password: process.env.PASSWORD || 'password',
 })
 app.use('/timeline', auth)
+app.use('/followers', auth)
+app.use('/followings', auth)
 app.use('/action', auth)
+
+
+const getSelfUser = async () => await prisma.user.findUnique({
+  where: { domain_screenName: { domain: DOMAIN, screenName: USERNAME } },
+})
 
 app.get('/', (c) => {
   return c.html(
@@ -146,16 +156,15 @@ app.get(`/@${USERNAME}`, getUserAction)
 app.get(`/users/${USERNAME}`, getUserAction)
 
 app.post(`/users/${USERNAME}/inbox`, async (c) => {
-  const body = await c.req.json()
-  switch (body.type) {
-    case 'Follow': {
-      const follow = body as {
-        type: 'Follow',
-        actor: string,
-        object: string,
-      }
+  var selfUser = getSelfUser()
 
-      if (follow.object !== `https://${DOMAIN}/users/${USERNAME}`) {
+  const body = await c.req.json()
+  switch (body["@type"]) {
+    case 'Follow': {
+      const actorId = body.actor["@id"]
+      const objectId = body.object["@id"]
+
+      if (objectId !== `https://${DOMAIN}/users/${USERNAME}`) {
         return c.json({
           error: 'invalid_object',
         }, 400)
@@ -164,13 +173,48 @@ app.post(`/users/${USERNAME}/inbox`, async (c) => {
       // (注意！) 送信元のユーザーの正当性を確認していない危険な実装になっているので、
       // ちゃんと実装するときにはHTTP Signatureを使って送信元のユーザーを確認してください。
 
-      //TODO: フォローをDBに保存する
+      const actorDomain = (new URL(actorId)).host
 
-      return c.json({
-        type: 'Accept',
-        actor: `https://${DOMAIN}/users/${USERNAME}`,
-        object: follow,
+      const actor: any = await ky.get(actorId, { headers: { "Accept": "application/activity+json" } }).json()
+      const actorUser = prisma.user.upsert({
+        where: { domain_screenName: { domain: actorDomain, screenName: actor.preferredUsername } },
+        create: {
+          screenName: actor.preferredUsername,
+          domain: actorDomain,
+          inbox: actor.inbox,
+          displayName: actor.name,
+          iconUrl: actor.icon.url,
+          publicKey: actor.publicKey.publicKeyPem,
+        },
+        update: {},
       })
+
+      prisma.follows.upsert({
+        where: {
+          follower_following: {
+            follower: selfUser,
+            following: actorUser,
+          },
+        },
+        create: {},
+        update: {},
+      })
+
+      // TODO: HTTP Signatureで署名する。JSONの中身も適当なのでなんとかする。
+      await ky.post(actor.inbox, {
+        json: {
+          "@context": "https://www.w3.org/ns/activitystreams",
+          "id": `https://${DOMAIN}/users/${USERNAME}/outbox/1`,
+          "type": "Accept",
+          "actor": `https://${DOMAIN}/users/${USERNAME}`,
+          "object": body,
+        },
+        headers: {
+          "Content-Type": "application/activity+json",
+        },
+      })
+
+      return c.json({})
     }
     default: {
       return c.json({
@@ -184,9 +228,11 @@ app.post('/action/follow', async (c) => {
   const body = await c.req.parseBody()
   switch (body.action) {
     case 'follow': {
+      // TODO: 実装する
       return c.json({})
     }
     case 'unfollow': {
+      // TODO: 実装する
       return c.json({})
     }
     default: {
@@ -197,17 +243,29 @@ app.post('/action/follow', async (c) => {
   }
 })
 
-app.get('/timeline', (c) => {
+app.get('/timeline', async (c) => {
+  var selfUser = getSelfUser()
+
   return c.html(<html>
     <head>
       <title>Timeline: An experimental implementation of ActivityPub (2023/12)</title>
     </head>
 
     <body>
+      <div style="display: flex; align-items: center;">
+        <img src="/static/icon.png" width="64" hegiht="64" style=" width: 64px; height: 64px; border: solid 1px #999; flex-shrink: 0;" />
+        <div>
+          <p>
+            @nullkal<br />
+            フォロワー: <a href="/followers">{await prisma.follows.count({ where: { follower: selfUser } })}人</a>,
+            フォロー中: <a href="/followings">{await prisma.follows.count({ where: { following: selfUser } })}人</a>
+          </p>
+        </div>
+      </div>
+
       <form action="/action/follow" method="POST">
         <input type="text" name="user" />
         <button type="submit" name="action" value="follow">フォロー</button>
-        <button type="submit" name="action" value="unfollow">フォロー解除</button>
       </form>
 
       <h2>タイムライン</h2>
