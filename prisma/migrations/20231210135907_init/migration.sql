90bfa700a827fc6aa45dabc042da7b3c5a3909cf
nullkal <nullkal@nil.nu>
2023-12-10 22:59:40 +0900

    Prismaをセットアップ

---
+++ prisma/migrations/20231210135907_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "screenName" TEXT NOT NULL,
+    "domain" TEXT NOT NULL,
+    "inbox" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "iconUrl" TEXT NOT NULL,
+    "publicKey" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Follows" (
+    "followerId" INTEGER NOT NULL,
+    "followingId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("followerId", "followingId"),
+    CONSTRAINT "Follows_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Follows_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "postedAt" DATETIME NOT NULL,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_domain_screenName_key" ON "User"("domain", "screenName");
---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+import * as fs from 'fs'
+
+const USERNAME = process.env.USERNAME || 'nullkal'
+const DOMAIN = process.env.DOMAIN || 'localhost'
+
+const PUBLIC_KEY = fs.readFileSync('./data/public.pem', 'utf8')
+
+const prisma = new PrismaClient()
+async function main() {
+  const selfUser = await prisma.user.upsert({
+    where: { domain_screenName: { domain: DOMAIN, screenName: USERNAME } },
+    update: {},
+    create: {
+      screenName: USERNAME,
+      domain: DOMAIN,
+      inbox: `https://${DOMAIN}/users/${USERNAME}/inbox`,
+      displayName: USERNAME,
+      iconUrl: `https://${DOMAIN}/static/icon.png`,
+      publicKey: PUBLIC_KEY,
+    },
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
